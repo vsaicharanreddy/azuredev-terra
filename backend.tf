8be51ea923f3689500e9b4d172edd11c081baaf1
vsaicharanreddy <119204401+vsaicharanreddy@users.noreply.github.com>
2022-12-08 11:43:11 +0530

    deleting db

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
-    key = "main"
+    bucket = "testing-backend-ttf"
+    key = "test.tfstate"
     region = "us-east-2"
-    dynamodb_table = "my-dynamodb-table"
   }
 }
