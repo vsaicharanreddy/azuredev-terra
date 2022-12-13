4faf09c309bae02ed52435418b301b90c3692569
vsaicharanreddy <119204401+vsaicharanreddy@users.noreply.github.com>
2022-12-13 11:03:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-020db2c14939a8efb" 
+    default     = "ami-0c12b5d624d73f1c0" 
 }
 variable "versioning" {
     type        = bool
